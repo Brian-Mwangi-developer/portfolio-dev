1d2a8f6a45b97af0c13d86cab3a0091c791277fb
Brian Mwangi <70840946+Brian-Mwangi-developer@users.noreply.github.com>
2026-07-08 06:34:17 +0300

    Add Mixpanel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,14 @@ import { Expertise } from "@/components/expertise";
 import Hero from "@/components/hero";
 import { Projects } from "@/components/projects";
 
+import mixpanel from "mixpanel-browser";
+
+// Create an instance of the Mixpanel object, your token is already added to this snippet
+mixpanel.init("29042c2256fe41de87ae7090222a4149", {
+  autocapture: true,
+  record_sessions_percent: 100,
+});
+
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-50">
